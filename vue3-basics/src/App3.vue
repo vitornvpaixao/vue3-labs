a8a77f5d711e341e142c10ec124c61b09a5d4a80
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-07 18:34:02 +0000

    feat(compoisiton api): add new feature & onMounted & fetching dummy data

--- vue3-basics/src/App3.vue
+++ vue3-basics/src/App3.vue
@@ -10,7 +10,15 @@
 
 	<h3 class="mt-2">Tasks:</h3>
 	<ul>
-		<li v-for="task in tasks" :key="task">{{ task }}</li>
+		<li v-for="(task, index) in tasks" :key="task">
+			<span>
+				{{ task }}
+			</span>
+
+			<button class="ml-4" @click="deleteTask(index)">
+				X
+			</button>
+		</li>
 	</ul>
 
 	<div class="mt-1">
@@ -26,15 +34,30 @@
 			Change Status
 		</button>
 	</div>
+
+	<div class="mt-4">
+		<form @submit.prevent="addTask">
+			<label for="newTask">Add Task</label>
+			<input
+				type="text"
+				id="newTask"
+				name="newTask"
+				v-model="newTask"
+			/>
+
+			<button type="submit">Submit</button>
+		</form>
+	</div>
 </template>
 
 <script setup>
-	import { ref } from 'vue';
+	import { ref, onMounted } from 'vue';
 
 	const userName = ref('John Djoras');
 	const userStatus = ref('pending');
 	const tasks = ref(['Task One', 'Task Two', 'Task Three']);
 	const link = ref('https://www.abola.pt');
+	const newTask = ref('');
 
 	const toggleBtnStatus = () => {
 		if (userStatus.value === 'active') {
@@ -45,6 +68,30 @@
 			userStatus.value = 'active';
 		}
 	};
+
+	const addTask = () => {
+		if (newTask.value.trim() !== '') {
+			tasks.value.push(newTask.value);
+			newTask.value = '';
+		}
+	};
+
+	const deleteTask = (index) => {
+		tasks.value.splice(index, 1);
+	};
+
+	onMounted(async () => {
+		try {
+			const response = await fetch(
+				'https://jsonplaceholder.typicode.com/todos',
+			);
+			const data = await response.json();
+
+			tasks.value = data.map((task) => task.title);
+		} catch (error) {
+			console.log('Error fetching tasks');
+		}
+	});
 </script>
 
 <style scoped>

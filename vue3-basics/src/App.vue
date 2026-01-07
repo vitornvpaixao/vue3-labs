d4882205946e7ca2cc2126783268d00cce5bbe0e
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-07 16:56:14 +0000

    feat(option api): clean proj and test some directives

--- vue3-basics/src/App.vue
+++ vue3-basics/src/App.vue
@@ -1,62 +1,64 @@
 <template>
-	<header>
-		<img
-			alt="Vue logo"
-			class="logo"
-			src="./assets/logo.svg"
-			width="125"
-			height="125"
-		/>
+	<h1>Vue Jobs - Web App</h1>
 
-		<div class="wrapper">
-			<HelloWorld
-				msg="You did it!"
-			/>
-		</div>
-	</header>
+	<h2>{{ userName }}</h2>
+	<p v-if="userStatus === 'active'">User is active</p>
+	<p v-else-if="userStatus === 'pending'">
+		User is pending
+	</p>
+	<p v-else="userStatus">User is inactive</p>
 
-	<main>
-		<TheWelcome />
-	</main>
+	<h3 class="mt-2">Tasks:</h3>
+	<ul>
+		<li v-for="task in tasks" :key="task">{{ task }}</li>
+	</ul>
+
+	<div class="mt-1">
+		<a :href="link">Click for Sports News</a>
+	</div>
+
+	<div class="d-flex flex-column">
+		<button v-on:click="toggleBtnStatus">
+			Change Status
+		</button>
+
+		<button class="mt-2" @click="toggleBtnStatus">
+			Change Status
+		</button>
+	</div>
 </template>
 
-<script setup>
-	import HelloWorld from './components/HelloWorld.vue';
-	import TheWelcome from './components/TheWelcome.vue';
-
-	const cenas = [
-		'cenas',
-		'unders',
-	];
+<script>
+	export default {
+		data() {
+			return {
+				userName: 'John Djoras',
+				userStatus: 'pending',
+				tasks: ['Task One', 'Task Two', 'Task Three'],
+				link: 'https://www.abola.pt',
+			};
+		},
+		methods: {
+			toggleBtnStatus() {
+				if (this.userStatus === 'active') {
+					this.userStatus = 'pending';
+				} else if (this.userStatus === 'pending') {
+					this.userStatus = 'inactive';
+				} else {
+					this.userStatus = 'active';
+				}
+			},
+		},
+	};
 </script>
 
 <style scoped>
-	header {
-		line-height: 1.5;
+	h1 {
+		color: red;
 	}
 
-	.logo {
-		display: block;
-		margin: 0 auto 2rem;
-	}
-
-	@media (min-width: 1024px) {
-		header {
-			display: flex;
-			place-items: center;
-			padding-right: calc(
-				var(--section-gap) / 2
-			);
-		}
-
-		.logo {
-			margin: 0 2rem 0 0;
-		}
-
-		header .wrapper {
-			display: flex;
-			place-items: flex-start;
-			flex-wrap: wrap;
-		}
+	h2 {
+		color: white;
+		font-weight: 700;
 	}
 </style>

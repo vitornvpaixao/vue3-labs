cc77a1ced257d6dd483db8903abfefca305bf3d9
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-12 20:32:31 +0000

    feat(UI): create homecard component

--- vue3-basics/src/App.vue
+++ vue3-basics/src/App.vue
@@ -1,9 +1,11 @@
 <template>
 	<Navbar />
 	<Hero />
+	<HomeCards />
 </template>
 
 <script setup>
 	import Navbar from '@/components/Navbar.vue';
 	import Hero from '@/components/Hero.vue';
+	import HomeCards from './components/HomeCards.vue';
 </script>
---
+++ vue3-basics/src/components/HomeCards.vue
@@ -0,0 +1,20 @@
+<template>
+	<section class="py-4">
+		<div class="container-xl lg:container m-auto">
+			<div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+				<div class="bg-gray-100 p-6 rounded-lg shadow-md">
+					<h2 class="text-2xl font-bold">For Developers</h2>
+					<p class="mt-2 mb-4">Browse our Vue jobs and start your carrer today</p>
+					<a href="#" class="rounded-lg bg-black text-white py-2 px-4 inline-block">Browse jobs</a>
+				</div>
+				<div class="bg-green-100 p-6 rounded-lg shadow-md">
+					<h2 class="text-2xl font-bold">For Employers</h2>
+					<p class="mt-2 mb-4">List your job to find the perfect developer for the role</p>
+					<a href="#" class="rounded-lg bg-green-400 text-white py-2 px-4 inline-block">Add Job</a>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup></script>

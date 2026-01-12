f19a38abb59c05143aef19e69bbddad7fd1197cd
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-12 14:29:34 +0000

    feat(UI): add hero and navbar components

--- vue3-basics/src/App.vue
+++ vue3-basics/src/App.vue
@@ -1,3 +1,9 @@
 <template>
-	<h1 class="text-2xl">Vue Jobs</h1>
+	<Navbar />
+	<Hero />
 </template>
+
+<script setup>
+	import Navbar from '@/components/Navbar.vue';
+	import Hero from '@/components/Hero.vue';
+</script>
---
+++ vue3-basics/src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+	<nav class="bg-green-700 border-b border-green-500">
+		<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+			<div class="flex h-20 items-center justify-between">
+				<div class="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+					<a class="flex flex-shrink-0 items-center justify-center mr-4" href="#">
+						<img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+						<span class="hidden md:block text-white text-2xl font-bold ml-2">Vue Jobs</span>
+					</a>
+
+					<div class="md:ml-auto h-10">
+						<div class="flex space-x-2">
+							<a
+								href="#"
+								class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+								>Home</a
+							>
+							<a
+								href="#"
+								class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+								>Jobs</a
+							>
+							<a
+								href="#"
+								class="text-white bg-green-900 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+								>Add Job</a
+							>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script setup>
+	import logo from '@/assets/img/logo.png';
+</script>

9a4a3aa3c62b278698e52a3f4b06ea8532da2d7f
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-12 21:16:49 +0000

    feat(Card): create reusable Card component and implement multiple data passing

---
+++ vue3-basics/src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+	<div :class="`${cardBgColor} p-6 rounded-lg shadow-md`">
+		<h2 v-if="cardTitle !== ''" class="text-2xl font-bold">{{ cardTitle }}</h2>
+		<p v-if="cardTitle !== ''" class="mt-2 mb-4">{{ cardDetails }}</p>
+		<a
+			v-if="cardTitle !== ''"
+			href="#"
+			:class="`${cardBtnColor} rounded-lg bg-black text-white py-2 px-4 inline-block`"
+			>{{ cardBtnDetails }}</a
+		>
+
+		<slot></slot>
+	</div>
+</template>
+
+<script setup>
+	import { defineProps } from 'vue';
+
+	defineProps({
+		cardTitle: {
+			type: String,
+			default: '',
+		},
+		cardDetails: {
+			type: String,
+			default: 'Card Details',
+		},
+		cardBtnDetails: {
+			type: String,
+			default: 'Card Button',
+		},
+		cardBgColor: {
+			type: String,
+			default: 'bg-gray-100',
+		},
+		cardBtnColor: {
+			type: String,
+			default: 'bg-black',
+		},
+	});
+</script>
--- vue3-basics/src/components/HomeCards.vue
+++ vue3-basics/src/components/HomeCards.vue
@@ -2,19 +2,49 @@
 	<section class="py-4">
 		<div class="container-xl lg:container m-auto">
 			<div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-				<div class="bg-gray-100 p-6 rounded-lg shadow-md">
+				<Card>
 					<h2 class="text-2xl font-bold">For Developers</h2>
 					<p class="mt-2 mb-4">Browse our Vue jobs and start your carrer today</p>
 					<a href="#" class="rounded-lg bg-black text-white py-2 px-4 inline-block">Browse jobs</a>
-				</div>
-				<div class="bg-green-100 p-6 rounded-lg shadow-md">
-					<h2 class="text-2xl font-bold">For Employers</h2>
-					<p class="mt-2 mb-4">List your job to find the perfect developer for the role</p>
-					<a href="#" class="rounded-lg bg-green-400 text-white py-2 px-4 inline-block">Add Job</a>
-				</div>
+				</Card>
+				<Card
+					card-bg-color="bg-green-100"
+					card-title="For Employers"
+					card-details="List your job to find the perfect developer for the role"
+					card-btn-details="Add Job"
+					card-btn-color="bg-green-400"
+				>
+				</Card>
+				<Card
+					:card-bg-color="cardInfo.bgColor"
+					:card-title="cardInfo.title"
+					:card-details="cardInfo.details"
+					:card-btn-details="cardInfo.btnDetails"
+					:card-btn-color="cardInfo.btnColor"
+				>
+				</Card>
 			</div>
 		</div>
 	</section>
 </template>
 
-<script setup></script>
+<script setup>
+	import { ref, reactive } from 'vue';
+	import Card from '@/components/Card.vue';
+
+	const cardInfo = ref({
+		title: 'For other Test',
+		details: 'For other type of card creation. Justo for fun and for other types of card creation',
+		bgColor: 'bg-blue-100',
+		btnDetails: 'Add Test',
+		btnColor: 'bg-blue-400',
+	});
+
+	const cardBtnChangeColor = () => {
+		setTimeout(() => {
+			cardInfo.value.btnColor = 'bg-red-400';
+		}, 3000);
+	};
+
+	cardBtnChangeColor();
+</script>

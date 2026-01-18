ff508c90790e9e908a13e15df146c62b36c1b89c
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-18 19:52:31 +0000

    feat(delete-job): add function to delete jobs

--- vue3-basics/src/views/JobView.vue
+++ vue3-basics/src/views/JobView.vue
@@ -80,6 +80,7 @@
 						</RouterLink>
 
 						<button
+							@click="deleteJob"
 							class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
 						>
 							Delete Job
@@ -93,12 +94,16 @@
 
 <script setup>
 	import { reactive, onMounted } from 'vue';
-	import { useRoute, RouterLink } from 'vue-router';
+	import { useRoute, RouterLink, useRouter } from 'vue-router';
+	import { useToast } from 'vue-toastification';
+
 	import BackButton from '@/components/BackButton.vue';
 	import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 	import axios from 'axios';
 
 	const route = useRoute();
+	const router = useRouter();
+	const toast = useToast();
 	const jobId = route.params.id;
 	const state = reactive({
 		job: {},
@@ -116,4 +121,19 @@
 			state.isLoading = false;
 		}
 	});
+
+	const deleteJob = async () => {
+		try {
+			const confirm = window.confirm('Are you sure you want to delete this job?');
+
+			if (confirm) {
+				await axios.delete(`/api/jobs/${jobId}`);
+				toast.success('Job Deleted Successfully');
+				router.push(`/jobs`);
+			}
+		} catch (error) {
+			console.error('Error deleting job', error);
+			toast.success('Job Was Not deleted');
+		}
+	};
 </script>

847e330166b21a015243d05ca8db11ac893057e6
Vitor Paixão <vitornvpaixao@gmail.com>
2026-01-20 20:56:36 +0000

    refactor(ts): normalize type and interface naming convention across codebase

--- typescript/interfaces.ts
+++ typescript/interfaces.ts
@@ -8,19 +8,19 @@ const user1 = {
 
 user1.age = 22; // there isnt an interface for user1 but if .age='22' js will decide that age is a number due to the definition of the object's prop value
 
-interface userInterface {
+interface UserInterface {
 	id: number;
 	name: string;
 	age?: number; // Using question mark means this props is not required or means can be number or undefined
 }
 
-const user: userInterface = {
+const user: UserInterface = {
 	id: 2,
 	name: 'Pedro',
 	age: 22, // Since we define this object as UserInterface, if we write value as a string it will an error immediately
 };
 
-const user2: userInterface = {
+const user2: UserInterface = {
 	// Without Error because we use a question mark in prop age. Object props can be required or not.
 	id: 2,
 	name: 'Pedro',
@@ -33,7 +33,7 @@ if (!user2.age) {
 	console.log(user2.age);
 }
 
-interface userInterface1 {
+interface UserInterface1 {
 	id: number;
 	name: string;
 	age?: number;
@@ -41,7 +41,7 @@ interface userInterface1 {
 	greeting(): void;
 }
 
-const user4: userInterface1 = {
+const user4: UserInterface1 = {
 	id: 2,
 	name: 'jhony',
 	age: 34,
